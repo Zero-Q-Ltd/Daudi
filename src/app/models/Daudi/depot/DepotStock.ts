b632e66d38ef5c1f317dc776badcd4d3b77d5e05
Kisinga <kamana@kisinga.family>
2020-01-15 13:02:44 +0300

    removed entry from stock quantity models

--- src/app/models/Daudi/depot/DepotStock.ts
+++ src/app/models/Daudi/depot/DepotStock.ts
@@ -3,18 +3,11 @@ export interface DepotStock {
         totalActive: number,
         used: number
     };
-    entry: {
-        totalActive: number,
-        used: number
-    };
 }
 export const EmptyDepotQty: DepotStock = {
     ase: {
         used: 0,
         totalActive: 0
     },
-    entry: {
-        used: 0,
-        totalActive: 0
-    }
+
 };
