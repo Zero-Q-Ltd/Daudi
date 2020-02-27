30d195765130211bb078c2fffdd1a946cd79a6d9
Kisinga <kamana@kisinga.family>
2020-02-27 14:12:35 +0300

    separated cloud functions stock collections

--- functions/src/validators/orderupdate.ts
+++ functions/src/validators/orderupdate.ts
@@ -3,6 +3,10 @@ import { QboOrder } from "../models/Qbo/QboOrder";
 import { Order } from "../models/Daudi/order/Order";
 import { editStats } from "../tasks/crud/daudi/editStats";
 import { ReadAndInstantiate } from "../tasks/crud/daudi/QboConfig";
+import { readStock, kpcStockCollection } from "../tasks/crud/daudi/Stock";
+import { Stock, newStock } from "../models/Daudi/omc/Stock";
+import { FuelNamesArray } from "../models/Daudi/fuel/FuelType";
+import { firestore } from "firebase-admin";
 
 export function validOrderUpdate(order: Order, omcId: string) {
     switch (order.stage) {
@@ -34,31 +38,36 @@ export function validOrderUpdate(order: Order, omcId: string) {
     }
 }
 export function validTruckUpdate(order: Order, omcId: string) {
-    switch (order.stage) {
-        case 3:
+    switch (order.truck.stage) {
+        /**
+         * This is a completed order
+         */
+        case 4:
+            const batch = firestore().batch();
+
             return editStats(order, "paid");
-        case 6:
-            /**
-             * Delete orders deleted on Daudi, which havae already been created on QB
-             */
-            if (order.QbConfig.QbId) {
-                console.log("deleting order...");
-                return ReadAndInstantiate(omcId).then(res => {
-                    return res.qbo.getInvoice(order.QbConfig.QbId).then(result => {
-                        const resultinvoice = result.Invoice as QboOrder;
-                        resultinvoice.void = true;
-                        /**
-                         * @todo Implement deletion reason and User detail
-                         */
-                        // resultinvoice.CustomerMemo = {
-                        //     value: order.stagedata["6"].data.reason
-                        // };
-                        return res.qbo.updateInvoice(resultinvoice);
-                    });
-                });
-            } else return true;
-            break;
         default:
             return true;
     }
+}
+
+async function adjustASE(omcId: string, order: Order, batch: FirebaseFirestore.WriteBatch) {
+    return await readStock(omcId).then(snapshot => {
+        const stockObject: Stock = { ...newStock(), ...snapshot.data() };
+        FuelNamesArray.forEach(fueltype => {
+            /**
+             * sum the total qty observed in each entry
+             */
+            const qtyToAdjust = order.fuel[fueltype].entries.reduce((a, b) => a + (b.qty - b.observed), 0);
+            stockObject.qty[fueltype].ase += qtyToAdjust;
+        });
+        batch.set(kpcStockCollection(omcId), stockObject);
+        return batch.commit();
+    });
+}
+async function adjustEntries(omcId: string, order: Order, batch: FirebaseFirestore.WriteBatch) {
+    const directory = firestore()
+        .collection("omc")
+        .doc(omcId)
+        .collection("entries");
 }
